978f0d3020956ed8b260a51184100015bbe825ba
Ashton K Ellis <ashtonkellis@gmail.com>
2018-06-21 20:12:44 -0700

    merge static method working

--- __test__/ll_merge.test.js
+++ __test__/ll_merge.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const LinkedList = require('./../04-ll-insertions/ll_insertions');
+// const util = require('util');
+const LinkedList = require('./../08-ll-merge/ll_merge');
 
 describe('Linked List #append', () => {
   let testLL;
@@ -27,11 +28,42 @@ describe('Linked List #append', () => {
   });
 });
 
+describe('Linked List #shift', () => {
+  let testLL;
+
+  beforeEach(() => {
+    testLL = new LinkedList();
+    testLL.append(1);
+    testLL.append(2);
+    testLL.append(3);
+  });
+
+  afterEach(() => {
+    testLL = null;
+  });
+
+  test('#shift: simple case', () => {
+    expect(testLL.shift().value).toBe(1);
+    expect(testLL.shift().value).toBe(2);
+    expect(testLL.shift().value).toBe(3);
+    // expect(testLL.shift()).toBeNull();
+  });
+});
+
 describe('Linked List #merge', () => {
   let testLL1;
   let testLL2;
   
   beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+    testLL1 = null;
+    testLL2 = null;
+  });
+
+  test('#merge: simple case', () => {
     testLL1 = new LinkedList();
     testLL1.append(1);
     testLL1.append(3);
@@ -41,25 +73,20 @@ describe('Linked List #merge', () => {
     testLL2.append(2);
     testLL2.append(4);
     testLL2.append(6);
-  });
-
-  afterEach(() => {
-    testLL1 = null;
-    testLL2 = null;
-  });
-
-  test('#merge: simple case', () => {
+    
     const mergedLL = LinkedList.merge(testLL1, testLL2);
-    console.log(testLL1);
-    console.log(testLL2);
-    console.log(mergedLL);
-
-    expect(mergedLL.head).toBe(1);
-    expect(mergedLL.head.next).toBe(2);
-    expect(mergedLL.head.next.next).toBe(3);
-    expect(mergedLL.head.next.next.next).toBe(4);
-    expect(mergedLL.head.next.next.next.next).toBe(5);
-    expect(mergedLL.head.next.next.next.next.next).toBe(6);
+    // console.log(util.inspect(mergedLL, false, null), 'merged linked list');
+    expect(mergedLL.head.value).toBe(1);
+    expect(mergedLL.head.next.value).toBe(2);
+    expect(mergedLL.head.next.next.value).toBe(3);
+    expect(mergedLL.head.next.next.next.value).toBe(4);
+    expect(mergedLL.head.next.next.next.next.value).toBe(5);
+    expect(mergedLL.head.next.next.next.next.next.value).toBe(6);
     expect(mergedLL.head.next.next.next.next.next.next).toBeNull();
+  });
+
+  test('#merge: empty lists', () => {
+    const mergedLL = LinkedList.merge(new LinkedList(), new LinkedList());
+    expect(mergedLL.head).toBeNull();
   });
 });
