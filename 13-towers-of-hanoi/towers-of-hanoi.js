a0229ae2845095dc7d53469fd2873400b3be1016
Ashton K Ellis <ashtonkellis@gmail.com>
2018-06-27 23:32:34 -0700

    not even close to done

---
+++ 13-towers-of-hanoi/towers-of-hanoi.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = class TowerOfHanoi() {
+  constructor(discQuantity) {
+    this.stackA = [];
+    this.stackB = [];
+    this.stackC = [];
+    this.stacks = [stackC, stackB, stackA];
+  }
+    
+  moveDiscs() {
+    while (stackA.length && stackB.length) {
+    }
+  }
+}
